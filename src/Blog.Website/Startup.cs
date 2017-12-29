7d3ac3491f5d34889990b881c0306cc1c120ad00
Per Manniche Bering <pmbering@gmail.com>
2017-12-29 16:28:47 +0100

    test

--- src/Blog.Website/Startup.cs
+++ src/Blog.Website/Startup.cs
@@ -1,8 +1,11 @@
-﻿using Blog.Website.Data;
+﻿using System.IO;
+using Blog.Website.Data;
 using Blog.Website.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace Blog.Website
@@ -25,6 +28,11 @@ namespace Blog.Website
 
             app.UseResponseCaching();
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(env.WebRootPath, @".well-known")),
+                RequestPath = new PathString("/.well-known")
+            });
 
             app.UseMiddleware<RobotsTxtMiddleware>();
             app.UseMiddleware<SitemapMiddleware>();
