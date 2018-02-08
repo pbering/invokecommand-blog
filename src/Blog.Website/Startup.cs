d2e849544b25ebfa8f3602ab3133600125fbda4d
Per Manniche Bering <pmbering@gmail.com>
2018-02-08 16:05:40 +0100

    testing

--- src/Blog.Website/Startup.cs
+++ src/Blog.Website/Startup.cs
@@ -1,9 +1,7 @@
-﻿using System.IO;
-using Blog.Website.Data;
+﻿using Blog.Website.Data;
 using Blog.Website.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -23,13 +21,6 @@ namespace Blog.Website
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-
-            using (var iisRewriteRules = File.OpenText(Path.Combine(env.ContentRootPath, "iis-rewrite-rules.xml")))
-            {
-                var options = new RewriteOptions().AddIISUrlRewrite(iisRewriteRules);
-
-                app.UseRewriter(options);
             }
 
             app.UseResponseCaching();
